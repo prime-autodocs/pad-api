ff4ca3205d69ebe6f9b39f08c3561c5ce11b6808
Lucas Mendonça <lms.devop@gmail.com>
2025-02-13 00:48:08 -0300

    Include middleware configs

--- interfaces/api/config.py
+++ interfaces/api/config.py
@@ -1,10 +1,19 @@
 import fastapi
+from fastapi.middleware.cors import CORSMiddleware
 
 from interfaces.api.routers.customers import router as customers
 from interfaces.api.routers.vehicles import router as vehicles
 from interfaces.api.routers.users import router as users
 
 app = fastapi.FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["https://pad-interface.vercel.app"],
+    allow_credentials=True,
+    allow_methods=["*"],  # Permite GET, POST, PUT, DELETE, etc.
+    allow_headers=["*"],  # Permite todos os headers
+)
 
 ## Routers
 app.include_router(customers, prefix="/customers", tags=["customers"])
